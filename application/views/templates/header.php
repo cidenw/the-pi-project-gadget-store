8d8bca93dddc31d0678dc7940896bce3fdefc273
Aris Timi Almoro <lowrenzalmoro@gmail.com>
2017-05-28 02:08:31 +0800

    Navbar added

---
+++ application/views/templates/header.php
@@ -0,0 +1,43 @@
+<html>
+	<head>
+		<title>The Pi Project</title>
+		<link href='assets/css/style.css' rel='stylesheet'>
+	</head>
+	<body>
+
+<<<<<<< HEAD
+<a href="<?php echo base_url(); ?>"><img src="<?php echo base_url(); ?>assets/images/logo2.png" name="logo" height="11%"></a>
+<br>
+<div class="navbar">
+<ul>
+  <li><a href="<?php echo base_url(); ?>">Home</a></li>
+  <li><a href="<?php echo base_url(); ?>about">About</a></li>
+  <li><a href="<?php echo base_url(); ?>products">Products</a></li>
+  <li><a href="<?php echo base_url(); ?>support">Support</a></li>
+  <li><input type="text" name="search" placeholder="Search.."></li>
+  <li><input type="submit" value="Find"></li>
+  <li style="float:right"><a href="<?php echo base_url(); ?>cart"><img class="cart" src="<?php echo base_url(); ?>assets/images/cart.png" height="25px"></a></li>
+=======
+	<nav class = "navbar navbar-inverse">
+		<div class = "container"> <!--
+			<div class = "navbar-header">
+				<a class = "navbar-brand" href = "#">TPP</a>
+			</div> -->
+			<img src="<?php echo base_url(); ?>assets/images/logo.png" name="logo" width="210" height="200">
+			
+			<div id ="navbar">
+				<ul class="nav navbar-nav" id="navbar-ul">
+					<li id="navbar-li"><a id="navbar-a" href="<?php echo base_url(); ?>">Home</a></li>
+					<li id="navbar-li"><a id="navbar-a" href="<?php echo base_url(); ?>about">About</a><li>
+					<li id="navbar-li"><a id="navbar-a" href="<?php echo base_url(); ?>products">Products</a><li>
+					<li id="navbar-li"><a id="navbar-a" href="<?php echo base_url(); ?>">Support</a><li>
+
+					
+				</ul>
+				<a href="<?php echo base_url()."cart/"?>">Shopping Cart (im in header)</a>
+			</div>
+>>>>>>> e271d1f9235bdf21615139b8101f73c9a9bb54c0
+
+</ul>
+</div>
+	
