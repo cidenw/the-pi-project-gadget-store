b971e6d2d58b5ae42d76976138134bf954321dc8
Waleed <waleed.occidental@gmail.com>
2017-05-31 19:50:28 +0800

    Sort doesnt reset search query.PDF Library added

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -1,74 +1,89 @@
 <?php
-	class Cart extends CI_Controller{
-		public function index(){
+class Cart extends CI_Controller{
+	public function index(){
 
-			$data = array();
-			$data['title'] = 'Shopping Cart';
-			$data['products'] = $this->product_model->get_products();
-			
-			$this->load->library('session');
-			$data['cart'] = $this->session->userdata('cart');
-			if(empty($data['cart'])){
-				$data['cart'] = array();
-			}
-			$this->load->view('templates/header');
-			$this->load->view('cart/index', $data);
-			$this->load->view('templates/footer');
-
+		$data = array();
+		$data['title'] = 'Shopping Cart';
+		$data['products'] = $this->product_model->get_products();
+		
+		$this->load->library('session');
+		$data['cart'] = $this->session->userdata('cart');
+		if(empty($data['cart'])){
+			$data['cart'] = array();
 		}
+		$this->load->view('templates/header');
+		$this->load->view('cart/index', $data);
+		$this->load->view('templates/footer');
 
-		public function add($productID = NULL){
-			if($productID == NULL){
-				show_404();
-			}
-
-			if($productID == "checkout"){
-				checkout();
-				return;
-			}
-			$data['product'] = $this->product_model->get_products($productID);
-			$data['title'] = $data['product']['productName'];
-
-			$this->load->library('session');
-			$cart = $this->session->userdata('cart');
-			if(empty($cart)){
-				$cart = array();
-			}
-			array_push($cart, (String)$data['product']['productID']);
-			$cart = array_count_values($cart);
-			$data['added'] = true;
-			$this->session->set_userdata('cart', $cart);
-			$this->load->view('templates/header');
-			$this->load->view('products/view', $data);
-			$this->load->view('templates/footer');
-		}
-
-		public function update(){
-			$data = array();
-			$data['title'] = 'Shopping Cart';
-			$data['products'] = $this->product_model->get_products();
-			
-			$this->load->library('session');
-			$data['cart'] = $this->session->userdata('cart');
-			if(empty($data['cart'])){
-				$data['cart'] = array();
-			}
-			$this->load->view('cart/update',$data);
-		}
-		public function checkout(){
-
-			
-			$data = array();
-			$data['title'] = 'Checkout';
-			$data['products'] = $this->product_model->get_products();
-			
-			$this->load->library('session');
-			$data['cart'] = $this->session->userdata('cart');
-			
-			$this->load->view('templates/header');
-			$this->load->view('cart/checkout', $data);
-			$this->load->view('templates/footer');
-			
-		}
 	}
+
+	public function add($productID = NULL){
+		if($productID == NULL){
+			show_404();
+		}
+
+		if($productID == "checkout"){
+			checkout();
+			return;
+		}
+		$data['product'] = $this->product_model->get_products($productID);
+		$data['title'] = $data['product']['productName'];
+
+		$this->load->library('session');
+		$cart = $this->session->userdata('cart');
+		if(empty($cart)){
+			$cart = array();
+		}
+		array_push($cart, (String)$data['product']['productID']);
+		$cart = array_count_values($cart);
+		$data['added'] = true;
+		$this->session->set_userdata('cart', $cart);
+		$this->load->view('templates/header');
+		$this->load->view('products/view', $data);
+		$this->load->view('templates/footer');
+	}
+
+	public function update(){
+		$data = array();
+		$data['title'] = 'Shopping Cart';
+		$data['products'] = $this->product_model->get_products();
+		
+		$this->load->library('session');
+		$data['cart'] = $this->session->userdata('cart');
+		if(empty($data['cart'])){
+			$data['cart'] = array();
+		}
+		$this->load->view('cart/update',$data);
+	}
+	public function checkout(){
+
+		$data = array();
+		$data['title'] = 'Checkout';
+		$data['products'] = $this->product_model->get_products();
+		
+		$this->load->library('session');
+		$data['cart'] = $this->session->userdata('cart');
+		
+		$this->load->library('Pdf');
+
+		$pdf = new Pdf('P', 'mm', 'A4', true, 'UTF-8', false);
+		$pdf->SetTitle('My Title');
+		$pdf->SetHeaderMargin(30);
+		$pdf->SetTopMargin(20);
+		$pdf->setFooterMargin(20);
+		$pdf->SetAutoPageBreak(true);
+		$pdf->SetAuthor('Author');
+		$pdf->SetDisplayMode('real', 'default');
+
+		$pdf->AddPage();
+
+		$pdf->Write(5, 'Some sample text');
+		$pdf->Output('My-File-Name.pdf', 'I');
+		$this->load->view('templates/header');
+		$this->load->view('cart/checkout', $data);
+		$this->load->view('templates/footer');
+
+		
+	}
+}
 ?>
--- application/views/products/index.php
+++ application/views/products/index.php
@@ -93,11 +93,11 @@
 <select onchange="if (this.value) window.location.href=this.value">
 	
     <option value="<?php echo base_url()."products" ?>">Pick one:</option>
-    <option value="<?php echo base_url()."products?orderby=brand" ?>"   <?php if($orderby=="brand") echo "selected"?> >Sort by: Brand</option>
-    <option value="<?php echo base_url()."products?orderby=price" ?>"  <?php if($orderby=="price") echo "selected"?> >Sort by Price(Low to High)</option>
-    <option value="<?php echo base_url()."products?orderby=price-desc" ?>"  <?php if($orderby=="price-desc") echo "selected"?> >Sort by Price(High to Low)</option>
-     <option value="<?php echo base_url()."products?orderby=category" ?>"  <?php if($orderby=="category") echo "selected"?> >Sort by Category</option>
-     <option value="<?php echo base_url()."products?orderby=name" ?>"  <?php if($orderby=="name") echo "selected"?> >Sort by Product Name</option>
+    <option value="<?php echo base_url()."products?orderby=brand"; if(isset($search)){echo "&search=".$search;}   ?>"   <?php if($orderby=="brand") echo "selected"?> >Sort by: Brand</option>
+    <option value="<?php echo base_url()."products?orderby=price"; if(isset($search)){echo "&search=".$search;}   ?>"  <?php if($orderby=="price") echo "selected"?> >Sort by Price(Low to High)</option>
+    <option value="<?php echo base_url()."products?orderby=price-desc"; if(isset($search)){echo "&search=".$search;}   ?>"  <?php if($orderby=="price-desc") echo "selected"?> >Sort by Price(High to Low)</option>
+     <option value="<?php echo base_url()."products?orderby=category"; if(isset($search)){echo "&search=".$search;}   ?>"  <?php if($orderby=="category") echo "selected"?> >Sort by Category</option>
+     <option value="<?php echo base_url()."products?orderby=name"; if(isset($search)){echo "&search=".$search;}   ?>"  <?php if($orderby=="name") echo "selected"?> >Sort by Product Name</option>
 </select>
 
 <br><br>
