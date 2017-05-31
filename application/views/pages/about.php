22ffca0fc57601f87fd2605ae7229f74b93e41d1
Joshua Villanueva <joshua.villanueva2017@gmail.com>
2017-05-31 14:48:21 +0800

    Added css on Products page and added text on About page

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,2 +1,11 @@
 <h2><?= $title ?></h2>
-This is about
+<p>
+<h2>What is The PI Project?</h2>
+	The PI Project, established on May 19,2017, is an e-shopping platform that is designed to help its consumers and buyers find their electronic needs in a whim. Likewise, the TPP also cares for its retailers and sellers such that we made sure that we close affordable yet 'win-win' deals. The TPP chose to venture online gadget selling because we believe that the need for electronic devices is fast-rising along with the technology and industry itself. We envision the future along the ranks of the known online shopping stores such as Lazada and Amazon and we see a stunning world with you!
+</p>
+<p>
+<h2>Why do we use it?</h2>
+	In The Pi Project, we believe that the consumers would have different attitions with regards to shopping physically. We have made it our top priority to provide items that we sell a detailed summary of what the gadgets boast. Also, we at TPP designed a well-developed shopping environment that is comparable to shopping at malls. It is a big-deal for us to make sure that transactions made under our service are safe, efficient, and worthwhile. We make sure that even how busy you are, wherever, whenever, you`ll get the latest and top-trend gadgets of the time! 
+</p>
+<h2>Where does it come from?</h2>
+	The Pi Project is based in the Philippines where hard-working millenials do exist. At TPP, we make the customers feel "Welcome" and that it`s "more fun at TPP!". Its founders are well-known for their good looks, optimistic attitude and of course, for the well-being and nature they have. 
--- application/views/products/index.php
+++ application/views/products/index.php
@@ -1,3 +1,94 @@
+
+<style>
+ .products-section{
+ 	position: relative;
+    max-width: 400px;
+    margin:-4px;
+    padding: 0;
+    display: inline-block;
+ }
+ .products-grid{
+    height: 380px;
+    overflow: auto;
+    margin: 0 0 0 0;
+    padding: 0;
+    list-style: none;
+    display: block;
+    text-align: center;
+    width: 100%
+ }
+
+ .products-grid:after{
+    clear:both;
+ }
+
+ .products-grid:after, .products-box:before{
+    content:"";
+    display:table;
+ }
+
+ .products-grid li{
+    overflow: hidden;
+    width: 328px;
+    height: 328px;
+    display: inline-block;
+    margin: 20px;
+ }
+
+ .products-box{
+    width: 100%;
+    height: 100%;
+    position: relative;
+    cursor: pointer;
+    border-radius: 10px;
+    -webkit-transition: 0.3s ease-in-out, -webkit-transform 0.3s ease-in-out;
+    -moz-transition: 0.3s ease-in-out, -moz-transform 0.3s ease-in-out;
+    transition: 0.3s ease-in-out, transform 0.3s ease-in-out;
+	opacity: 0.75;
+
+ }
+
+ .products-box:hover {
+    transform: scale(1.05);
+ } 
+
+ .products-img-1 {
+    background: linear-gradient(rgba(0,0,0,0.50), rgba(0,0,0,0.10));
+	transition: all 0.3s ease-in;
+}
+
+ .products-img-1:hover{
+    transition: all 0.3s ease-in;
+    opacity: 1;
+ }
+
+ .products-info h3{
+ 	text-align: left;
+    font-family: 'Century Gothic';
+    font-weight: 400;
+    color: #fff;
+    font-size: 42px;
+    margin: 0 30px;
+    padding: 100px 0 0 0;
+    line-height: 1.5;
+ }
+
+ .products-info p{
+    margin: 10 10px;
+    font-family: 'Century Gothic';
+    color: #fff;
+ }
+
+ li a {
+    font-family: 'Century Gothic';
+ }
+
+ li a:hover{
+    background: transparent;
+ }
+
+</style>
+
 <h2><?= $title ?></h2>
 <select onchange="if (this.value) window.location.href=this.value">
 	
@@ -9,21 +100,28 @@
      <option value="<?php echo base_url()."products?orderby=name" ?>"  <?php if($orderby=="name") echo "selected"?> >Sort by Product Name</option>
 </select>
 
+<br><br>
+
+<center>
 <?php 
-	
-	
-
-foreach ($products as $product) : ?>
-
-	<?php 
-	//if($product->type=="accesories"){ 
-		echo $product['productID']." - "?>
-		<a href="<?php echo site_url("/products/".$product['productID']); ?>"><?php echo $product->productName?> </a>
-	 <br>
-	<?php
-	//}
-	?>
-
-	
+	foreach ($products as $product) : ?>
+	<div class = "products-section">
+		<ul class = "products-grid">
+		<li>
+			<div class = "products-box products-img-1 ">
+			<a href="<?php echo site_url("/products/".$product['productID']); ?>">
+				<h3><?php echo $product -> productName?></h3>	
+				<img src="<?php echo base_url()."assets/images/products/".$product['productID'].".jpg" ?>" class = "products-box products-img-1"/>
+				</a>
+				<br>
+			<?php
+			//}
+			?>
+			</div>
+		</li>
+		</div>
+	</ul>
+	</div>
 <?php endforeach;?>
 
+</center>
